a12d40d8514b2caf64c59adb7c7752fa17df2227
msowa <msowa@live.co.uk>
2021-10-01 15:20:19 +0100

    envfactors edit

--- db/data/ingredients.js
+++ db/data/ingredients.js
@@ -1,305 +1,305 @@
 export default [
   // carbon footprint - 'GHG emissions per kilogram(Poore & Nemecek, 2018)': 
-  //land use -  'Land use per kilogram'
-  //water use - 'Freshwater withdrawals per kilogram'
-  //scarcity-weighted water use - 'Scarcity-weighted water use per kilogram'
-  //water pollution - 'Eutrophying emissions per kilogram'
-  // {
-  //   'ingredient': 'Apples',
-  //   'GHG emissions per kilogram': 0.43,
-  //   'Land use per kilogram': 0.63,
-  //   'Freshwater withdrawals per kilogram': 180.1,
-  //   'Scarcity-weighted water use per kilogram': 12948.6,
-  //   'Eutrophying emissions per kilogram': 1.45
-  // },
-  // {
-  //   'ingredient': 'Bananas',
-  //   'GHG emissions per kilogram': 0.86,
-  //   'Land use per kilogram': 1.93,
-  //   'Freshwater withdrawals per kilogram': 114.5,
-  //   'Scarcity-weighted water use per kilogram': 661.9,
-  //   'Eutrophying emissions per kilogram': 3.29
-  // },
-  // {
-  //   'ingredient': 'Barley',
-  //   'GHG emissions per kilogram': 1.18,
-  //   'Land use per kilogram': 1.11,
-  //   'Freshwater withdrawals per kilogram': 17.1,
-  //   'Scarcity-weighted water use per kilogram': 696.4,
-  //   'Eutrophying emissions per kilogram': 2.33
-  // },
-  // {
-  //   'ingredient': 'Beef (beef herd)',
-  //   'GHG emissions per kilogram': 99.48,
-  //   'Land use per kilogram': 326.21,
-  //   'Freshwater withdrawals per kilogram': 1451.2,
-  //   'Scarcity-weighted water use per kilogram': 34732.5,
-  //   'Eutrophying emissions per kilogram': 301.41
-  // },
-  // {
-  //   'ingredient': 'Beef (dairy herd)',
-  //   'GHG emissions per kilogram': 33.3,
-  //   'Land use per kilogram': 43.24,
-  //   'Freshwater withdrawals per kilogram': 2714.3,
-  //   'Scarcity-weighted water use per kilogram': 119805.2,
-  //   'Eutrophying emissions per kilogram': 365.29
-  // },
-  // {
-  //   'ingredient': 'Beet Sugar',
-  //   'GHG emissions per kilogram': 1.81,
-  //   'Land use per kilogram': 1.83,
-  //   'Freshwater withdrawals per kilogram': 217.7,
-  //   'Scarcity-weighted water use per kilogram': 9493.3,
-  //   'Eutrophying emissions per kilogram': 5.41
-  // },
-  // {
-  //   'ingredient': 'Berries & Grapes',
-  //   'GHG emissions per kilogram': 1.53,
-  //   'Land use per kilogram': 2.41,
-  //   'Freshwater withdrawals per kilogram': 419.6,
-  //   'Scarcity-weighted water use per kilogram': 21162.1,
-  //   'Eutrophying emissions per kilogram': 6.12
-  // },
-  // {
-  //   'ingredient': 'Cabbage (Brassicas)',
-  //   'GHG emissions per kilogram': 0.51,
-  //   'Land use per kilogram': 0.55,
-  //   'Freshwater withdrawals per kilogram': 119.4,
-  //   'Scarcity-weighted water use per kilogram': 8455.1,
-  //   'Eutrophying emissions per kilogram': 5.01
-  // },
-  // {
-  //   'ingredient': 'Cane Sugar',
-  //   'GHG emissions per kilogram': 3.2,
-  //   'Land use per kilogram': 2.04,
-  //   'Freshwater withdrawals per kilogram': 620.1,
-  //   'Scarcity-weighted water use per kilogram': 16438.6,
-  //   'Eutrophying emissions per kilogram': 16.92
-  // },
-  // {
-  //   'ingredient': 'Cassava',
-  //   'GHG emissions per kilogram': 1.32,
-  //   'Land use per kilogram': 1.81,
-  //   'Freshwater withdrawals per kilogram': 0,
-  //   'Scarcity-weighted water use per kilogram': 0,
-  //   'Eutrophying emissions per kilogram': 0.69
-  // },
-  // {
-  //   'ingredient': 'Cheese',
-  //   'GHG emissions per kilogram': 23.88,
-  //   'Land use per kilogram': 87.79,
-  //   'Freshwater withdrawals per kilogram': 5605.2,
-  //   'Scarcity-weighted water use per kilogram': 180850.6,
-  //   'Eutrophying emissions per kilogram': 98.37
-  // },
-  // {
-  //   'ingredient': 'Citrus Fruit',
-  //   'GHG emissions per kilogram': 0.39,
-  //   'Land use per kilogram': 0.86,
-  //   'Freshwater withdrawals per kilogram': 82.7,
-  //   'Scarcity-weighted water use per kilogram': 4662.7,
-  //   'Eutrophying emissions per kilogram': 2.24
-  // },
-  // {
-  //   'ingredient': 'Coffee',
-  //   'GHG emissions per kilogram': 28.53,
-  //   'Land use per kilogram': 21.62,
-  //   'Freshwater withdrawals per kilogram': 25.9,
-  //   'Scarcity-weighted water use per kilogram': 337,
-  //   'Eutrophying emissions per kilogram': 110.52
-  // },
-  // {
-  //   'ingredient': 'Dark Chocolate',
-  //   'GHG emissions per kilogram': 46.65,
-  //   'Land use per kilogram': 68.96,
-  //   'Freshwater withdrawals per kilogram': 540.6,
-  //   'Scarcity-weighted water use per kilogram': 2879.2,
-  //   'Eutrophying emissions per kilogram': 87.08
-  // },
-  // {
-  //   'ingredient': 'Eggs',
-  //   'GHG emissions per kilogram': 4.67,
-  //   'Land use per kilogram': 6.27,
-  //   'Freshwater withdrawals per kilogram': 577.7,
-  //   'Scarcity-weighted water use per kilogram': 17982.7,
-  //   'Eutrophying emissions per kilogram': 21.76
-  // },
-  // {
-  //   'ingredient': 'Fish (farmed)',
-  //   'GHG emissions per kilogram': 13.63,
-  //   'Land use per kilogram': 8.41,
-  //   'Freshwater withdrawals per kilogram': 3691.3,
-  //   'Scarcity-weighted water use per kilogram': 41572.2,
-  //   'Eutrophying emissions per kilogram': 235.12
-  // },
-  // {
-  //   'ingredient': 'Groundnuts',
-  //   'GHG emissions per kilogram': 3.23,
-  //   'Land use per kilogram': 9.11,
-  //   'Freshwater withdrawals per kilogram': 1852.3,
-  //   'Scarcity-weighted water use per kilogram': 61797.9,
-  //   'Eutrophying emissions per kilogram': 14.14
-  // },
-  // {
-  //   'ingredient': 'Lamb & Mutton',
-  //   'GHG emissions per kilogram': 39.72,
-  //   'Land use per kilogram': 369.81,
-  //   'Freshwater withdrawals per kilogram': 1802.8,
-  //   'Scarcity-weighted water use per kilogram': 141925,
-  //   'Eutrophying emissions per kilogram': 97.13
-  // },
-  // {
-  //   'ingredient': 'Maize',
-  //   'GHG emissions per kilogram': 1.7,
-  //   'Land use per kilogram': 2.94,
-  //   'Freshwater withdrawals per kilogram': 215.7,
-  //   'Scarcity-weighted water use per kilogram': 10863.3,
-  //   'Eutrophying emissions per kilogram': 4.03
-  // },
-  // {
-  //   'ingredient': 'Milk',
-  //   'GHG emissions per kilogram': 3.15,
-  //   'Land use per kilogram': 8.95,
-  //   'Freshwater withdrawals per kilogram': 628.2,
-  //   'Scarcity-weighted water use per kilogram': 19786.3,
-  //   'Eutrophying emissions per kilogram': 10.65
-  // },
-  // {
-  //   'ingredient': 'Nuts',
-  //   'GHG emissions per kilogram': 0.43,
-  //   'Land use per kilogram': 12.96,
-  //   'Freshwater withdrawals per kilogram': 4133.8,
-  //   'Scarcity-weighted water use per kilogram': 229889.8,
-  //   'Eutrophying emissions per kilogram': 19.15
-  // },
-  // {
-  //   'ingredient': 'Oatmeal',
-  //   'GHG emissions per kilogram': 2.48,
-  //   'Land use per kilogram': 7.6,
-  //   'Freshwater withdrawals per kilogram': 482.4,
-  //   'Scarcity-weighted water use per kilogram': 18786.2,
-  //   'Eutrophying emissions per kilogram': 11.23
-  // },
-  // {
-  //   'ingredient': 'Onions & Leeks',
-  //   'GHG emissions per kilogram': 0.5,
-  //   'Land use per kilogram': 0.39,
-  //   'Freshwater withdrawals per kilogram': 14.3,
-  //   'Scarcity-weighted water use per kilogram': 932,
-  //   'Eutrophying emissions per kilogram': 3.24
-  // },
-  // {
-  //   'ingredient': 'Other Fruit',
-  //   'GHG emissions per kilogram': 1.05,
-  //   'Land use per kilogram': 0.89,
-  //   'Freshwater withdrawals per kilogram': 153.5,
-  //   'Scarcity-weighted water use per kilogram': 9533.1,
-  //   'Eutrophying emissions per kilogram': 2.43
-  // },
-  // {
-  //   'ingredient': 'Other Pulses',
-  //   'GHG emissions per kilogram': 1.79,
-  //   'Land use per kilogram': 15.57,
-  //   'Freshwater withdrawals per kilogram': 435.7,
-  //   'Scarcity-weighted water use per kilogram': 22477.4,
-  //   'Eutrophying emissions per kilogram': 17.08
-  // },
-  // {
-  //   'ingredient': 'Other Vegetables',
-  //   'GHG emissions per kilogram': 0.53,
-  //   'Land use per kilogram': 0.38,
-  //   'Freshwater withdrawals per kilogram': 102.5,
-  //   'Scarcity-weighted water use per kilogram': 4911.4,
-  //   'Eutrophying emissions per kilogram': 2.27
-  // },
-  // {
-  //   'ingredient': 'Peas',
-  //   'GHG emissions per kilogram': 0.98,
-  //   'Land use per kilogram': 7.46,
-  //   'Freshwater withdrawals per kilogram': 396.6,
-  //   'Scarcity-weighted water use per kilogram': 27948.2,
-  //   'Eutrophying emissions per kilogram': 7.52
-  // },
-  // {
-  //   'ingredient': 'Pork',
-  //   'GHG emissions per kilogram': 12.31,
-  //   'Land use per kilogram': 17.36,
-  //   'Freshwater withdrawals per kilogram': 1795.8,
-  //   'Scarcity-weighted water use per kilogram': 66867.4,
-  //   'Eutrophying emissions per kilogram': 76.38
-  // },
-  // {
-  //   'ingredient': 'Potatoes',
-  //   'GHG emissions per kilogram': 0.46,
-  //   'Land use per kilogram': 0.88,
-  //   'Freshwater withdrawals per kilogram': 59.1,
-  //   'Scarcity-weighted water use per kilogram': 2754.2,
-  //   'Eutrophying emissions per kilogram': 3.48
-  // },
-  // {
-  //   'ingredient': 'Poultry Meat',
-  //   'GHG emissions per kilogram': 9.87,
-  //   'Land use per kilogram': 12.22,
-  //   'Freshwater withdrawals per kilogram': 660,
-  //   'Scarcity-weighted water use per kilogram': 14177.9,
-  //   'Eutrophying emissions per kilogram': 48.7
-  // },
-  // {
-  //   'ingredient': 'Prawns',
-  //   'GHG emissions per kilogram': 26.87,
-  //   'Land use per kilogram': 2.97,
-  //   'Freshwater withdrawals per kilogram': 3515.4,
-  //   'Scarcity-weighted water use per kilogram': 127259,
-  //   'Eutrophying emissions per kilogram': 227.22
-  // },
-  // {
-  //   'ingredient': 'Rice',
-  //   'GHG emissions per kilogram': 4.45,
-  //   'Land use per kilogram': 2.8,
-  //   'Freshwater withdrawals per kilogram': 2248.4,
-  //   'Scarcity-weighted water use per kilogram': 49576.3,
-  //   'Eutrophying emissions per kilogram': 35.07
-  // },
-  // {
-  //   'ingredient': 'Root Vegetables',
-  //   'GHG emissions per kilogram': 0.43,
-  //   'Land use per kilogram': 0.33,
-  //   'Freshwater withdrawals per kilogram': 28.4,
-  //   'Scarcity-weighted water use per kilogram': 929.2,
-  //   'Eutrophying emissions per kilogram': 1.61
-  // },
-  // {
-  //   'ingredient': 'Soymilk',
-  //   'GHG emissions per kilogram': 0.98,
-  //   'Land use per kilogram': 0.66,
-  //   'Freshwater withdrawals per kilogram': 27.8,
-  //   'Scarcity-weighted water use per kilogram': 955.6,
-  //   'Eutrophying emissions per kilogram': 1.06
-  // },
-  // {
-  //   'ingredient': 'Tofu (soybeans)',
-  //   'GHG emissions per kilogram': 3.16,
-  //   'Land use per kilogram': 3.52,
-  //   'Freshwater withdrawals per kilogram': 148.6,
-  //   'Scarcity-weighted water use per kilogram': 5113.2,
-  //   'Eutrophying emissions per kilogram': 6.16
-  // },
-  // {
-  //   'ingredient': 'Tomatoes',
-  //   'GHG emissions per kilogram': 2.09,
-  //   'Land use per kilogram': 0.8,
-  //   'Freshwater withdrawals per kilogram': 369.8,
-  //   'Scarcity-weighted water use per kilogram': 5335.7,
-  //   'Eutrophying emissions per kilogram': 7.51
-  // },
-  // {
-  //   'ingredient': 'Wheat & Rye',
-  //   'GHG emissions per kilogram': 1.57,
-  //   'Land use per kilogram': 3.85,
-  //   'Freshwater withdrawals per kilogram': 647.5,
-  //   'Scarcity-weighted water use per kilogram': 33385.6,
-  //   'Eutrophying emissions per kilogram': 7.16
-  // },
+  // land use -  landUse
+  // water use - waterUse
+  // scarcity-weighted water use - scarcityWeightedWaterUse
+  // water pollution - waterPollution
+  {
+    ingredient: 'Apples',
+    carbonFootprint: 0.43,
+    landUse: 0.63,
+    waterUse: 180.1,
+    scarcityWeightedWaterUse: 12948.6,
+    waterPollution: 1.45
+  },
+  {
+    ingredient: 'Bananas',
+    carbonFootprint: 0.86,
+    landUse: 1.93,
+    waterUse: 114.5,
+    scarcityWeightedWaterUse: 661.9,
+    waterPollution: 3.29
+  },
+  {
+    ingredient: 'Barley',
+    carbonFootprint: 1.18,
+    landUse: 1.11,
+    waterUse: 17.1,
+    scarcityWeightedWaterUse: 696.4,
+    waterPollution: 2.33
+  },
+  {
+    ingredient: 'Beef (beef herd)',
+    carbonFootprint: 99.48,
+    landUse: 326.21,
+    waterUse: 1451.2,
+    scarcityWeightedWaterUse: 34732.5,
+    waterPollution: 301.41
+  },
+  {
+    ingredient: 'Beef (dairy herd)',
+    carbonFootprint: 33.3,
+    landUse: 43.24,
+    waterUse: 2714.3,
+    scarcityWeightedWaterUse: 119805.2,
+    waterPollution: 365.29
+  },
+  {
+    ingredient: 'Beet Sugar',
+    carbonFootprint: 1.81,
+    landUse: 1.83,
+    waterUse: 217.7,
+    scarcityWeightedWaterUse: 9493.3,
+    waterPollution: 5.41
+  },
+  {
+    ingredient: 'Berries & Grapes',
+    carbonFootprint: 1.53,
+    landUse: 2.41,
+    waterUse: 419.6,
+    scarcityWeightedWaterUse: 21162.1,
+    waterPollution: 6.12
+  },
+  {
+    ingredient: 'Cabbage (Brassicas)',
+    carbonFootprint: 0.51,
+    landUse: 0.55,
+    waterUse: 119.4,
+    scarcityWeightedWaterUse: 8455.1,
+    waterPollution: 5.01
+  },
+  {
+    ingredient: 'Cane Sugar',
+    carbonFootprint: 3.2,
+    landUse: 2.04,
+    waterUse: 620.1,
+    scarcityWeightedWaterUse: 16438.6,
+    waterPollution: 16.92
+  },
+  {
+    ingredient: 'Cassava',
+    carbonFootprint: 1.32,
+    landUse: 1.81,
+    waterUse: 0,
+    scarcityWeightedWaterUse: 0,
+    waterPollution: 0.69
+  },
+  {
+    ingredient: 'Cheese',
+    carbonFootprint: 23.88,
+    landUse: 87.79,
+    waterUse: 5605.2,
+    scarcityWeightedWaterUse: 180850.6,
+    waterPollution: 98.37
+  },
+  {
+    ingredient: 'Citrus Fruit',
+    carbonFootprint: 0.39,
+    landUse: 0.86,
+    waterUse: 82.7,
+    scarcityWeightedWaterUse: 4662.7,
+    waterPollution: 2.24
+  },
+  {
+    ingredient: 'Coffee',
+    carbonFootprint: 28.53,
+    landUse: 21.62,
+    waterUse: 25.9,
+    scarcityWeightedWaterUse: 337,
+    waterPollution: 110.52
+  },
+  {
+    ingredient: 'Dark Chocolate',
+    carbonFootprint: 46.65,
+    landUse: 68.96,
+    waterUse: 540.6,
+    scarcityWeightedWaterUse: 2879.2,
+    waterPollution: 87.08
+  },
+  {
+    ingredient: 'Eggs',
+    carbonFootprint: 4.67,
+    landUse: 6.27,
+    waterUse: 577.7,
+    scarcityWeightedWaterUse: 17982.7,
+    waterPollution: 21.76
+  },
+  {
+    ingredient: 'Fish (farmed)',
+    carbonFootprint: 13.63,
+    landUse: 8.41,
+    waterUse: 3691.3,
+    scarcityWeightedWaterUse: 41572.2,
+    waterPollution: 235.12
+  },
+  {
+    ingredient: 'Groundnuts',
+    carbonFootprint: 3.23,
+    landUse: 9.11,
+    waterUse: 1852.3,
+    scarcityWeightedWaterUse: 61797.9,
+    waterPollution: 14.14
+  },
+  {
+    ingredient: 'Lamb & Mutton',
+    carbonFootprint: 39.72,
+    landUse: 369.81,
+    waterUse: 1802.8,
+    scarcityWeightedWaterUse: 141925,
+    waterPollution: 97.13
+  },
+  {
+    ingredient: 'Maize',
+    carbonFootprint: 1.7,
+    landUse: 2.94,
+    waterUse: 215.7,
+    scarcityWeightedWaterUse: 10863.3,
+    waterPollution: 4.03
+  },
+  {
+    ingredient: 'Milk',
+    carbonFootprint: 3.15,
+    landUse: 8.95,
+    waterUse: 628.2,
+    scarcityWeightedWaterUse: 19786.3,
+    waterPollution: 10.65
+  },
+  {
+    ingredient: 'Nuts',
+    carbonFootprint: 0.43,
+    landUse: 12.96,
+    waterUse: 4133.8,
+    scarcityWeightedWaterUse: 229889.8,
+    waterPollution: 19.15
+  },
+  {
+    ingredient: 'Oatmeal',
+    carbonFootprint: 2.48,
+    landUse: 7.6,
+    waterUse: 482.4,
+    scarcityWeightedWaterUse: 18786.2,
+    waterPollution: 11.23
+  },
+  {
+    ingredient: 'Onions & Leeks',
+    carbonFootprint: 0.5,
+    landUse: 0.39,
+    waterUse: 14.3,
+    scarcityWeightedWaterUse: 932,
+    waterPollution: 3.24
+  },
+  {
+    ingredient: 'Other Fruit',
+    carbonFootprint: 1.05,
+    landUse: 0.89,
+    waterUse: 153.5,
+    scarcityWeightedWaterUse: 9533.1,
+    waterPollution: 2.43
+  },
+  {
+    ingredient: 'Other Pulses',
+    carbonFootprint: 1.79,
+    landUse: 15.57,
+    waterUse: 435.7,
+    scarcityWeightedWaterUse: 22477.4,
+    waterPollution: 17.08
+  },
+  {
+    ingredient: 'Other Vegetables',
+    carbonFootprint: 0.53,
+    landUse: 0.38,
+    waterUse: 102.5,
+    scarcityWeightedWaterUse: 4911.4,
+    waterPollution: 2.27
+  },
+  {
+    ingredient: 'Peas',
+    carbonFootprint: 0.98,
+    landUse: 7.46,
+    waterUse: 396.6,
+    scarcityWeightedWaterUse: 27948.2,
+    waterPollution: 7.52
+  },
+  {
+    ingredient: 'Pork',
+    carbonFootprint: 12.31,
+    landUse: 17.36,
+    waterUse: 1795.8,
+    scarcityWeightedWaterUse: 66867.4,
+    waterPollution: 76.38
+  },
+  {
+    ingredient: 'Potatoes',
+    carbonFootprint: 0.46,
+    landUse: 0.88,
+    waterUse: 59.1,
+    scarcityWeightedWaterUse: 2754.2,
+    waterPollution: 3.48
+  },
+  {
+    ingredient: 'Poultry Meat',
+    carbonFootprint: 9.87,
+    landUse: 12.22,
+    waterUse: 660,
+    scarcityWeightedWaterUse: 14177.9,
+    waterPollution: 48.7
+  },
+  {
+    ingredient: 'Prawns',
+    carbonFootprint: 26.87,
+    landUse: 2.97,
+    waterUse: 3515.4,
+    scarcityWeightedWaterUse: 127259,
+    waterPollution: 227.22
+  },
+  {
+    ingredient: 'Rice',
+    carbonFootprint: 4.45,
+    landUse: 2.8,
+    waterUse: 2248.4,
+    scarcityWeightedWaterUse: 49576.3,
+    waterPollution: 35.07
+  },
+  {
+    ingredient: 'Root Vegetables',
+    carbonFootprint: 0.43,
+    landUse: 0.33,
+    waterUse: 28.4,
+    scarcityWeightedWaterUse: 929.2,
+    waterPollution: 1.61
+  },
+  {
+    ingredient: 'Soymilk',
+    carbonFootprint: 0.98,
+    landUse: 0.66,
+    waterUse: 27.8,
+    scarcityWeightedWaterUse: 955.6,
+    waterPollution: 1.06
+  },
+  {
+    ingredient: 'Tofu (soybeans)',
+    carbonFootprint: 3.16,
+    landUse: 3.52,
+    waterUse: 148.6,
+    scarcityWeightedWaterUse: 5113.2,
+    waterPollution: 6.16
+  },
+  {
+    ingredient: 'Tomatoes',
+    carbonFootprint: 2.09,
+    landUse: 0.8,
+    waterUse: 369.8,
+    scarcityWeightedWaterUse: 5335.7,
+    waterPollution: 7.51
+  },
+  {
+    ingredient: 'Wheat & Rye',
+    carbonFootprint: 1.57,
+    landUse: 3.85,
+    waterUse: 647.5,
+    scarcityWeightedWaterUse: 33385.6,
+    waterPollution: 7.16
+  },
   {
     ingredient: 'Wine',
     carbonFootprint: 1.79,
--- models/environmentalFactor.js
+++ models/environmentalFactor.js
@@ -4,7 +4,7 @@ const envFactorSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   totalGraph: { type: String, required: true },
-  supplyChainGraph: { type: String, required: true },
+  secondGraphic: { type: String, required: true },
   description: { type: String, required: true }
 })
 
