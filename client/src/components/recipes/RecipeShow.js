70a12979a58ae0b21b57c6c6d3640eca9e9d082d
Victor Liew <victorliew0711@gmail.com>
2021-10-08 11:56:56 +0100

    recipeindex -envfactors update

--- client/src/components/recipes/RecipeShow.js
+++ client/src/components/recipes/RecipeShow.js
@@ -42,22 +42,36 @@ const RecipeShow = ({ growingTree }) => {
               <img src={recipe.image} alt={recipe.name} className='col-12'/>
             </div>
             <div className='col-12 col-md-6'>
-              <div className ='environmental-impact text-center'>
+              <div className ='environmental-factors text-center'>
                 <h2>Environmental Impact</h2>
                 <div className='factors d-flex justify-content-between'>
-                  <div className ='CO2'>
-                    Carbon Footprint
-                    <p>{recipe.ingredients.reduce((total,ingredient) => total += ingredient.carbonFootprint, 0)}</p>
-                  </div>
-                  <div className ='Water'>
-                    Water Usage
-                    <p>{recipe.ingredients.reduce((total,ingredient) => total += ingredient.waterUse, 0)}</p>
-                  </div>
-                  <div className ='Land'>
-                    Land Use
-                    <p>{recipe.ingredients.reduce((total,ingredient) => total += ingredient.landUse, 0)}</p>
-                  </div>
+                  <Link to={'/environmentalfactors/615eea9353fed8cb12ab3314'} className ='btn'>
+                    <img src='https://res.cloudinary.com/mesowa/image/upload/v1633432484/Green%20Plate/co2-image_txzm1d.svg' alt='co2-image'/>
+                    <p>Carbon Footprint</p>
+                    <p>{recipe.ingredients.reduce((total,ingredient) => total += ingredient.carbonFootprint, 0)}kg</p>
+                  </Link>
+                  <Link to={'/environmentalfactors/615eea9353fed8cb12ab3315'} className ='btn'>
+                    <img src='https://res.cloudinary.com/mesowa/image/upload/v1633432484/Green%20Plate/land-use_dki2em.png' alt='land-image'/>
+                    <p>Land Use</p>
+                    <p>{recipe.ingredients.reduce((total,ingredient) => total += ingredient.landUse, 0)}m<sup>2</sup></p>
+                  </Link>
+                  <Link to={'/environmentalfactors/615eea9353fed8cb12ab3316'} className ='btn'>
+                    <img src='https://res.cloudinary.com/mesowa/image/upload/v1633432324/Green%20Plate/water-image_seblhm.png' alt='water-image'/>
+                    <p>Water Usage</p>
+                    <p>{recipe.ingredients.reduce((total,ingredient) => total += ingredient.waterUse, 0)}kg</p>
+                  </Link>
+                  <Link to={'/environmentalfactors/615eea9353fed8cb12ab3318'} className ='btn'>
+                    <img src='https://res.cloudinary.com/mesowa/image/upload/v1633432570/Green%20Plate/Screenshot_2021-10-05_at_12.16.00_jf1kjx.png' alt='pollution-image'/>
+                    <p>Water Pollution</p>
+                    <p>{recipe.ingredients.reduce((total,ingredient) => total += ingredient.waterPollution, 0)}g</p>
+                  </Link>
+                  <Link to={'/environmentalfactors/615eea9353fed8cb12ab3317'} className ='btn'>
+                    <img src='https://res.cloudinary.com/mesowa/image/upload/v1633432613/Green%20Plate/Screenshot_2021-10-05_at_12.16.43_vuhbc4.png' alt='scarcity-image'/>
+                    <p>Water Scarcity</p>
+                    <p>{recipe.ingredients.reduce((total,ingredient) => total += ingredient.scarcityWeightedWaterUse, 0)}kg</p>
+                  </Link>
                 </div>
+                <h6><em>Measurements are per kilogram of food product</em></h6>
                 <hr />
               </div>
               <h4><span>🍽</span> Key Ingredients</h4>
@@ -72,12 +86,12 @@ const RecipeShow = ({ growingTree }) => {
               <h4><span>🧑‍🍳</span> Added by</h4>
               <p><Link to={`/users/${recipe.owner._id}`}>{recipe.owner.username}</Link></p>
               <hr />
-              <Link to='/recipes' className='btn btn-lg'>Back to recipes</Link>
+              <Link to='/recipes' className='btn btn-primary orange-button'>Back to recipes</Link>
             </div>
           </div>
-          <div className='comment-section container row mt-4 border'>
+          {/* <div className='comment-section container row mt-4 border'>
             <p>Comments Section</p>
-          </div>
+          </div> */}
         </div>
         
         :
